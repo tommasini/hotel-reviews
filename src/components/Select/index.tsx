e91ebfd87dcfaad80c261cc350d29db0681a0fb0
Tomás SANTOS <tommasini15@gmail.com>
2022-03-24 22:52:35 +0000

    initial commit

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+export default function BasicSelect() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={"happy"}>Happy</MenuItem>
+          <MenuItem value={"unhappy"}>Unhappy</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
